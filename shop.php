258a34524293fb25f2c41411d21e7d0f988a0185
Plamena Peykova <plamena.peykova.2017@uni.strath.ac.uk>
2019-11-18 11:24:50 +0000

    header added and css

--- shop.php
+++ shop.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="uikit-3.2.3\css\uikit.min.css" />
     <script src="uikit-3.2.3\js\uikit.min.js"></script>
     <script src="uikit-3.2.3\js\uikit-icons.min.js"></script>
-    <title>Home</title>
+    <title>Shop</title>
     <script>
     </script>
 </head>
