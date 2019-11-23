1cf9864b32f5a335282c02e58f4d560cbf16089a
mcalexneill <57774142+mcalexneill@users.noreply.github.com>
2019-11-23 14:25:08 +0000

    Home, MyAccount & Login
    The Home.php is literally just filled with some basic information as you asked. The MyAccount.php does also as asked, displays items being sold by users and items sold, the items sold doesn't appear yet since there is nothing within the database and I didn't want to mess about with it too much but if it's populated with appropriate data it should work as expected. I've also added a very minor change to the login just to allow for session variables between MyAccount and Login.

---
+++ Home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="uikit-3.2.3/css/uikit.min.css" />
+    <script src="uikit-3.2.3/js/uikit.min.js"></script>
+    <script src="uikit-3.2.3/js/uikit-icons.min.js"></script>
+    <title>Home</title>
+
+</head>
+<body>
+<div>
+    <?php
+    include('header.php'); ?>
+</div>
+
+<p>Most people don't realize how much food they throw away every day — from uneaten leftovers to spoiled produce. More than 96 percent of the food we throw away ends up in landfills. In 2011, we  put more than 36 million tons of food waste in the landfill. Once in landfills, food breaks down to produce methane, a potent greenhouse gas which contributes to climate change.</p>
+
+<p><b>Benefits of Reducing Wasted Food:</b></p>
+<p>• Saves money from buying less food.</p>
+<p>• Reduces methane emissions from landfills and lowers your carbon footprint.</p>
+<p>• Conserves energy and resources, preventing pollution involved in the growing, manufacturing, transporting, and selling food (not to mention hauling the food waste and then putting it in the landfill).</p>
+<p>• Supports your community by providing donated untouched food that would have otherwise gone to waste to those who might not have a steady food supply.</p>
+
+<p><b>Here are a few tips to help you get started:</b></p>
+
+<p>• Plan ahead and buy only what you need. Going to the store without a plan or on an empty stomach can lead to buying more than we need. To keep your kitchen on track, try to eat leftovers, think of meals you might eat out, and avoid unnecessary purchases by planning your grocery list ahead of time. </p>
+
+<p>• Use your freezer. While there are plenty of benefits to eating fresh food, frozen foods can be just as nutritious. They also stay edible for much longer. A lot of seafood, for example, is frozen before it reaches your supermarket and then thawed and put on display. That means it will only stay fresh for a few days. By buying frozen seafood, you can extend the shelf life of the product considerably. Cooking and freezing food—especially produce—before it goes bad is a great way to avoid having to toss it.</p>
+
+<p>• Be creative with leftovers. Before you shop, use the food you already have. Websites like Big Oven, Supercook, and MyFridgeFood allow you to search for recipes based on ingredients already in your kitchen. You can also use apps like Epicurious and Allrecipes to make the most of what's in your fridge and pantry.</p>
+
+<p>• Blend, bake, or boil. Fruits and vegetables that are beyond ripe may not look pretty, but that doesn't mean they can't still taste delicious in recipes. Try using your wilting, browning, or imperfect produce to make sweet smoothies, bread, jams, sauces, or soup stocks.</p>
+
+<p>• Talk it up. Preventing food waste is the most effective way to shrink its impact on the planet. If we avoid producing food that we don't eat, we can save the land, water, and energy that would have been used to make it. And awareness is a good first step; according to ReFED, educating consumers about food waste could prevent 2.3 million tons of greenhouse gas emissions.</p>
+
+</body>
+</html>
+
---
+++ MyAccount.php
@@ -0,0 +1,145 @@
+<?php
+require ('connect.php');
+$categoryType = "";
+$mainArea = "";
+if(isset($_POST['submit'])){
+    if(isset($_POST['category'])) {
+        $categoryType = $_POST['category'];
+    }
+    if(isset($_POST['area'])) {
+        $mainArea = $_POST['area'];
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="uikit-3.2.3\css\uikit.min.css" />
+    <script src="uikit-3.2.3\js\uikit.min.js"></script>
+    <script src="uikit-3.2.3\js\uikit-icons.min.js"></script>
+    <title>My Account</title>
+    <style>
+        img {
+            border: 1px solid black;
+            background-color: black;
+            border-radius: 1px;
+            padding: 2px;
+            height: 200px;
+        }
+    </style>
+    <title>Home</title>
+
+</head>
+<body>
+<div>
+    <?php
+    include('header.php');
+
+    $userName = $_SESSION['name'];
+    echo "<p>Name: <b>".$userName."</b></p>";
+    
+    ?>
+</div>
+
+<h1>Items in the Marketplace</h1>
+
+<?php
+
+function select(){
+    $userId = $_SESSION['id'];
+    return "SELECT * FROM `Products` WHERE `user_id =` '.$userId.'";
+}
+
+function listProducts($array){
+echo "</br>";
+echo "<table>\n";
+    for ($j=0; $j < count($array); $j = $j + 3) {
+    echo "<tr>\n";
+        for($c=0; $c < 3; $c++) {
+        if(!empty($array[$j + $c])) {
+        $id = $array[$j + $c]['id'];
+        $image = $array[$j + $c]['picture'];
+        $description = $array[$j + $c]['description'];
+        $price = $array[$j + $c]['offprice'];
+        echo "<br><td><img src='data:image/jpeg;base64," . base64_encode($image) . "'/>" . "</br> $description </br> £ $price </br> <button> Add to basket </button></td>\n";
+        }
+        }
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+}
+
+$array = array();
+if((($categoryType == "all") || empty($categoryType)) && (($mainArea == "all") || (empty($mainArea)))) {
+    $sql = select();
+}
+$result = $conn->query($sql);
+if (!$result) {
+    die("Query failed.");
+}
+unset($array);
+$array = array();
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $array[] = $row;
+}
+if ($result->rowCount() > 0) {
+    listProducts($array);
+} else {
+    echo "No products available of this criteria. Try again later.";
+}
+?>
+
+<h1>Items Sold</h1>
+
+<?php
+
+function select1(){
+    $userId = $_SESSION['id'];
+    return "SELECT * FROM `soldProducts` WHERE `user_id =` '.$userId.'";
+}
+
+//function select1(){
+ //   return "SELECT * FROM `soldProducts` WHERE `user_id`= 2";
+//}
+
+function listProducts1($array){
+    echo "</br>";
+    echo "<table>\n";
+    for ($j=0; $j < count($array); $j = $j + 3) {
+        echo "<tr>\n";
+        for($c=0; $c < 3; $c++) {
+            if(!empty($array[$j + $c])) {
+                $id = $array[$j + $c]['id'];
+                $image = $array[$j + $c]['picture'];
+                $description = $array[$j + $c]['description'];
+                $price = $array[$j + $c]['offprice'];
+                echo "<br><td><img src='data:image/jpeg;base64," . base64_encode($image) . "'/>" . "</br> $description </br> £ $price </br> <button> Add to basket </button></td>\n";
+            }
+        }
+        echo "</tr>\n";
+    }
+    echo "</table>\n";
+}
+
+$array = array();
+if((($categoryType == "all") || empty($categoryType)) && (($mainArea == "all") || (empty($mainArea)))) {
+    $sql = select();
+}
+$result = $conn->query($sql);
+if (!$result) {
+    die("Query failed.");
+}
+unset($array);
+$array = array();
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $array[] = $row;
+}
+if ($result->rowCount() > 0) {
+    listProducts($array);
+} else {
+    echo "No products available of this criteria. Try again later.";
+}
+?>
