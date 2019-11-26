cb5d0c4fa70f9ca6a20f4abe2d98b808a9b49af4
mcalexneill <57774142+mcalexneill@users.noreply.github.com>
2019-11-26 15:32:48 +0000

    Update MyAccount.php

--- MyAccount.php
+++ MyAccount.php
@@ -69,7 +69,7 @@ echo "<table>\n";
         $image = $array[$j + $c]['picture'];
         $description = $array[$j + $c]['description'];
         $price = $array[$j + $c]['offprice'];
-        echo "<br><td><img src='data:image/jpeg;base64," . base64_encode($image) . "'/>" . "</br> $description </br> £ $price </br> <button> Add to basket </button></td>\n";
+        echo "<br><td><img src='data:image/jpeg;base64," . base64_encode($image) . "'/>" . "</br> $description </br> £ $price </br></td>\n";
         }
         }
         echo "</tr>\n";
