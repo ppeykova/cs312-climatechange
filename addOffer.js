e222537c652d202240668440a9240c5a1b582e7e
ppeykova <45003915+ppeykova@users.noreply.github.com>
2019-12-02 18:23:38 +0000

    Update addOffer.js

--- addOffer.js
+++ addOffer.js
@@ -1,136 +1,136 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="addOffer.js"></script>
-    <script>
-        var maxPrices = [5, 10, 5, 10, 5, 10, 5, 15, 5, 5];
-        function submitForm()
-        {
-            document.offerForm.submit();
-        }
-    </script>
-    <title>Add Offer</title>
-</head>
-<body class="profile-page sidebar-collapse">
-<div class="page-header header-filter" data-parallax="true" style="background-image: url('material-kit-master/assets/img/greenpic.jpg')">
-    <div class="container">
-    <?php require('header1.php'); ?>
-        <div class="brand text-center">
-            <h1>Add an Offer</h1>
-        </div>
-</div>
-</div>
-<div class="main main-raised">
-    <div class="container">
-        <div class="col-8 ml-auto mr-auto">
-        <div class="section">
-        <?php
-        $_SESSION['output_message'] = "";
-        $categories = ["Beans", "Bakery", "Dairy", "Drinks", "Fruit", "Meat", "Nuts", "Ready Meals", "Sweets", "Vegetables"];
-        if(!isset($_SESSION['email']))
-        {
-            echo "<div class='text-center'>";
-            echo "<p id='loginMessage'>Error: User is not logged in</p><br/>
-                  <p>Please <a href='login.php'>Login</a> first.</p>";
-            die();
-            echo "</div>";
-        }
-        if(isset($_POST['category']))
-        {
-            sendToDB();
-        }
-        function sendToDB()
-        {
-            if(!empty($_FILES['image']['tmp_name']))
-                $image = file_get_contents($_FILES['image']['tmp_name']);
-            else
-                $image = "";
-            $category = $_POST['category'];
-            $retailPrice = $_POST['retailPrice'];
-            $offerPrice = $_POST['offerPrice'];
-            $description = $_POST['description'];
-            echo $_POST['geocodeLatitude'].", ".$_POST['geocodeLongitude'];
-            $address = $_POST['addrStreet'].', '.$_POST['addrCity'].', '.$_POST['addrPostcode'];
-            include 'connect.php';
-            $stmt = $conn->prepare("INSERT INTO `products` (`picture`, `category`, `description`, `genprice`, `offprice`, `addr_latitude`, `addr_longitude`, `address`, `email`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            if($stmt->execute([$image, $category, $description, $retailPrice, $offerPrice, $_POST['geocodeLatitude'], $_POST['geocodeLongitude'], $address, $_SESSION['email']]) === true)
-                $_SESSION['output_message'] = "Offer added successfully!";
-            else
-                $_SESSION['output_message'] = "Error: Offer could not be added, please try again...";
-        }
-        ?>
-            <div class="col-md-7 ml-auto mr-auto">
-                <form name="offerForm" method="POST" action="" enctype="multipart/form-data" class="form">
+function uploadImage(input)
+{
+    var reader;
+    if (input.files && input.files[0])
+    {
+        var preview = document.getElementById("preview");
+        reader = new FileReader();
+        reader.onload = function (e) { preview.setAttribute('src', e.target.result); }
+        reader.readAsDataURL(input.files[0]);
+        preview.style.display = "inline";
+    }
+}
 
-                    <div class="container">
-                    <input type="hidden" name="geocodeLatitude" id="geocodeLatitude">
-                    <input type="hidden" name="geocodeLongitude" id="geocodeLongitude">
-                    <p><?php if(isset($_POST['category'])) { echo $_SESSION['output_message']; } ?></p>
-                    <p>Required fields marked with <span style="color: red;"/>*</span></p>
-                    <div class="section">
-                        <div class="text-center">
-                            <h2>Offer Details</h2>
-                        </div>
-                        <p><label for="image">
-                                <span class="card-text">Image: </span> <input type="file" id="image" name="image" accept="image/*" onchange="uploadImage(this)" required> <img src="" id="preview" class="card-img">
-                                <span class="error" id="imageMessage"></span>
-                            </label></p>
-                        <p><label for="category">
-                                <select id="category" name="category" class="custom-select" required>
-                                    <option value="0">Please select</option>
-                                    <?php
-                                    for($i = 0; $i < count($categories); $i++)
-                                        echo "<option value='".$categories[$i]."'>".$categories[$i]."</option>";
-                                    ?>
-                                </select>
-                                <span class="error" id="categoryMessage">*</span>
-                            </label></p>
-                        <p><label for="retailPrice">
-                                <input type="number" class="form-control" id="retailPrice" name="retailPrice" min="0" step=".01" placeholder="Retail Price:">
-                                <span class="error" id="retailPriceMessage">*</span>
-                            </label></p>
-                        <p><label for="offerPrice">
-                                <input type="number" id="offerPrice" class="form-control" name="offerPrice" min="0" step=".01" placeholder="Offer Price:">
-                                <span class="error" id="offerPriceMessage">*</span>
-                            </label></p>
-                        <p><label for="description">
-                                <textarea id="description" name="description" cols="40" rows="4" placeholder="Description:"></textarea>
-                                <span class="error" id="descriptionMessage">*</span>
-                            </label></p>
-                    </div>
-                    </div>
-                    <div class="section">
-                        <div class="text-center">
-                            <h2>Address Details</h2>
-                        </div>
-                        <p><label for="addrStreet">
-                                <input class="form-control" type="text" id="addrStreet" name="addrStreet" placeholder="Street:">
-                                <span class="error" id="addrStreetMessage">*</span>
-                            </label></p>
-                        <p><label for="addrCity">
-                                <input class="form-control" type="text" id="addrCity" name="addrCity" placeholder="City/Town:">
-                                <span class="error" id="addrCityMessage">*</span>
-                            </label></p>
-                        <p><label for="addrPostcode">
-                                <input type="text" class="form-control" id="addrPostcode" name="addrPostcode" placeholder="Postcode">
-                                <span class="error" id="addrPostcodeMessage">*</span>
-                            </label></p>
-                    </div>
+function validate()
+{
+    var valid = true;
+    var image = document.getElementById("image");
+    var category = document.getElementById("category");
+    var retailPrice = document.getElementById("retailPrice");
+    var offerPrice = document.getElementById("offerPrice");
+    var description = document.getElementById("description");
+    var addrStreet = document.getElementById("addrStreet");
+    var addrCity = document.getElementById("addrCity");
+    var addrPostcode = document.getElementById("addrPostcode");
 
-                    <p><label><button type="button" id="toHome" name="toHome"  class="btn btn-primary" onclick="window.location.href = 'home.php';">Return to Home</button></label>
-                        <label><button type="button" id="addBtn" name="addBtn" class="btn btn-primary" onclick="validate()">Add Offer</button></label></p>
+    document.getElementById("imageMessage").textContent = "*";
+    document.getElementById("categoryMessage").textContent = "*";
+    document.getElementById("retailPriceMessage").textContent = "*";
+    document.getElementById("offerPriceMessage").textContent = "*";
+    document.getElementById("descriptionMessage").textContent = "*";
+    document.getElementById("addrStreetMessage").textContent = "*";
+    document.getElementById("addrCityMessage").textContent = "*";
+    document.getElementById("addrPostcodeMessage").textContent = "*";
 
-                </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<?php
-require('footer.php');
-?>
-</body>
-</html>
+    if(image.value == 0)
+    {
+        document.getElementById("imageMessage").textContent = "* No image selected";
+        valid = false;
+    }
+    if(category.options[category.selectedIndex].value == 0)
+    {
+        document.getElementById("categoryMessage").textContent = "* No category selected";
+        valid = false;
+    }
+    else if(offerPrice.value > maxPrices[category.selectedIndex - 1])
+    {
+        document.getElementById("offerPriceMessage").textContent = "* Offer price is too high for the selected category (Max price: £" + maxPrices[category.selectedIndex - 1] + ")";
+        valid = false;
+    }
+
+    if(retailPrice.value == 0)
+    {
+        document.getElementById("retailPriceMessage").textContent = "* Retail price is required";
+        valid = false;
+    }
+    if(offerPrice.value == 0)
+    {
+        document.getElementById("offerPriceMessage").textContent = "* Offer price is required";
+        valid = false;
+    }
+    if(description.value == "")
+    {
+        document.getElementById("descriptionMessage").textContent = "* Description is required";
+        valid = false;
+    }
+    if(description.value.length > 255)
+    {
+        document.getElementById("descriptionMessage").textContent = "* Description must be less than 255 characters";
+        valid = false;
+    }
+    if(addrStreet.value == "")
+    {
+        document.getElementById("addrStreetMessage").textContent = "* Street address is required";
+        valid = false;
+    }
+    if(addrCity.value == "")
+    {
+        document.getElementById("addrCityMessage").textContent = "* City is required";
+        valid = false;
+    }
+    if(addrPostcode.value == "")
+    {
+        document.getElementById("addrPostcodeMessage").textContent = "* Postcode is required";
+        valid = false;
+    }
+    if(valid)
+    {
+        var address = addrStreet.value + ", " + addrCity.value + ", " + addrPostcode.value;
+        //alert(address);
+        geocode(address);
+    }
+}
+
+function geocode(query)
+{
+    $.ajax(
+        {
+            url: 'https://api.opencagedata.com/geocode/v1/json',
+            method: 'GET',
+            data:
+                {
+                    'key': '0bd6d81685ca456d862472ceadb767af',
+                    'q': query,
+                    'no_annotations': 1
+                    // see other optional params:
+                    // https://opencagedata.com/api#forward-opt
+                },
+            dataType: 'json',
+            statusCode:
+                {
+                    200: function(response)
+                    {  // success
+                        if(response.results.length === 0)
+                        {
+                            console.log("Address could not be found");
+
+                            document.getElementById("addrStreetMessage").textContent = "* Address could not be found";
+                        }
+                        else
+                        {
+                            console.log(response.results[0]['geometry']);
+                            document.getElementById("geocodeLatitude").setAttribute('value', response.results[0]['geometry']['lat']);
+                            document.getElementById("geocodeLongitude").setAttribute('value', response.results[0]['geometry']['lng']);
+                            submitForm();
+                        }
+                    },
+                    402: function()
+                    {
+                        console.log('hit free-trial daily limit');
+                        console.log('become a customer: https://opencagedata.com/pricing');
+                    }
+                    // other possible response codes:
+                    // https://opencagedata.com/api#codes
+                }
+        });
+}
