27611035b5ea6ac30f351b3881f69aa9e3a71272
ppeykova <45003915+ppeykova@users.noreply.github.com>
2019-12-02 18:25:11 +0000

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 <html>
 <?php
 require('connect.php');
-require('header1.php');
 
 /*LOG IN PAGE*/
 
@@ -10,42 +9,39 @@ require('header1.php');
 <head><title>Log in</title>
 
 </head>
-<body>
-
+<body class="login-page sidebar-collapse">
 <?php
-echo "<div class='container p-lg-5'>";
+echo "<div class='page-header header-filter' style='background-image: url('/material-kit-master/assets/img/landing.jpg'); background-size: cover; background-position: center center;'> ";
+echo "<div class='container'>";
 echo "<div class='row'>";
-echo "<div class='col-md-5 mx-auto'>
-			<div class='form-control-plaintext'>";
-echo "<div class='col-md-12 text-center'>
-      <h1>Log in</h1></div>";
-
-echo "<form method='POST' action=''>
-      <div class='form-group'>
-      <label for='exampleInputEmail1'>Email</label>
-      <input type='email' name='email' class='form-control' id='exampleInputEmail1' placeholder='Enter email' required>
+require('header1.php');
+echo  "<div class='col-lg-4 col-md-6 ml-auto mr-auto'>
+			<div class='card card-login'>
+                <form class='form'  method='POST' action='' name='myform' onsubmit='return checkform();'>
+              <div class='card-header card-header-primary text-center'>
+                    <h4 class='card-title'>Log in</h4>
+              </div>
+              <div class='card-body'>
+                <div class='input-group'>
+                  <input type='email' class='form-control' name='email' placeholder='Email...' required>
+                </div>
+                <div class='input-group'>
+                  <input type='password' class='form-control' name='password' placeholder='Password...' required>
+                </div>
+              </div>
+                <div class='footer text-center'>
+                   <input type='submit' value='Log in' name='submit' class='btn btn-primary btn-link btn-wd btn-lg'>
+                </div>
+              <div class='bmd-form-group'>
+                  <p class='text-center'>Forgotten password? <a href='resetpw.php'>Reset your password</a> </p>
+                  <p class='text-center'>You have not made an account? Just <a href='signup.php'>sign up</a> now.</p>
+                </div>
+            </form>
+          </div>
+        </div>
       </div>
-      <div class='form-group'>
-      <label for='exampleInputPassword1'>Password</label>
-      <input type='password' name='password' class='form-control' id='exampleInputPassword1'  placeholder='Password' required>
-      </div>
-      <div class='text-center'>
-      <button type='submit' name='submit' class='btn btn-block mybtn btn-primary tx-tfm'>Log in</button>
-      </div>
-      </form><br/>";
-echo "<div class='form-group'>
-    <div class='col-md-12 md-5 mx-auto'>
-        <p class='text-center'>Forgotten password? <a href='resetpw.php'>Reset your password</a> </p>
-        <p class='text-center'>You have not made an account? Just <a href='signup.php'>sign up</a> now.</p>
     </div>
-    </div>";
-echo '</div>
-    </div>
-    </div>
-    </div>
-    </div>';
-
-
+  </div>";
 
 //log in
 if(isset($_POST['submit'])){
@@ -77,6 +73,9 @@ if(isset($_POST['submit'])){
         echo "</div>";
     }
 }//end if form button click
+?>
+<?php
+require('footer.php');
 ?>
 </body>
 </html
