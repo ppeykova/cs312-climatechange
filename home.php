47c4a2a040a66c64f9922df59d9ebf6701d9d261
Krysr <57724604+Krysr@users.noreply.github.com>
2019-12-03 15:04:17 +0000

    Add files via upload

---
+++ home.php
@@ -0,0 +1,174 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+</head>
+<body class="landing-page sidebar-collapse">
+<div class="page-header header-filter" data-parallax="true" style="background-image: url('material-kit-master/assets/img/homep.jpg')">
+    <div class="container">
+        <div class="row">
+            <?php
+            require('header1.php'); ?>
+            <div class="col-md-8 ml-auto mr-auto">
+                <div class="brand text-md-left">
+                    <h1>Climate change</h1>
+                    <h4 class="title text-md-left">Climate change is a terrible problem, but you can help fight it by shopping on Wastood.</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="main main-raised">
+    <div class="container">
+        <div class="section text-center">
+            <div class="row">
+                <div class="col-md-8 ml-auto mr-auto">
+                    <h2 class="title">About us</h2>
+                    <h5 class="description">At Wastood, we take climate change very seriously. On way to tackle it is to shop locally because your food will travel a smaller distance meaning less greenhouse emissions associated with transport. The advantage of shopping with us is that food producers' offers are always at a reduced price, saving you a considerable amount of money. If you wish to add your own products to sell, it only takes a few minutes!</h5>
+                </div>
+            </div>
+            <div class="features">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="info">
+                            <div class="icon icon-info">
+                                <i class="material-icons">thumb_up</i>
+                            </div>
+                            <h4 class="info-title">Reduced price</h4>
+                            <p>All the products sold on this plateform are at a reduced price, you won't find cheaper food elsewhere.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="info">
+                            <div class="icon icon-success">
+                                <i class="material-icons">check_circle</i>
+                            </div>
+                            <h4 class="info-title">Organic food available</h4>
+                            <p>Some of the producers offer organic-only food so we cater for all users' need.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="info">
+                            <div class="icon icon-info">
+                                <i class="material-icons">tag_faces</i>
+                            </div>
+                            <h4 class="info-title">Selling food</h4>
+                            <p>If you have fresh products to add to the website to sell, you can do so in just a few clicks.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section text-center">
+            <h2 class="title">Here is our users</h2>
+            <div class="team">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="team-player">
+                            <div class="card card-plain">
+                                <div class="col-md-6 ml-auto mr-auto">
+                                    <img src="material-kit-master/assets/img/faces/avatar.jpg" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                                </div>
+                                <h4 class="card-title">Natalie Thomson
+                                    <br>
+                                    <small class="card-description text-muted">Buyer</small>
+                                </h4>
+                                <div class="card-body">
+                                    <p class="card-description">Natalie is a new buyer, she really cares about the future of our planet and decided to take action by buying her food locally, Wastood enables her to do just that!</p>
+                                </div>
+                                <div class="card-footer justify-content-center">
+                                    <a href="#" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                                    <a href="#" class="btn btn-link btn-just-icon"><i class="fa fa-instagram"></i></a>
+                                    <a href="#" class="btn btn-link btn-just-icon"><i class="fa fa-facebook-square"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="team-player">
+                            <div class="card card-plain">
+                                <div class="col-md-6 ml-auto mr-auto">
+                                    <img src="material-kit-master/assets/img/faces/christian.jpg" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                                </div>
+                                <h4 class="card-title">Jamie Ball
+                                    <br>
+                                    <small class="card-description text-muted">Buyer</small>
+                                </h4>
+                                <div class="card-body">
+                                    <p class="card-description">Jamie has been buying products on this site for a year now. "It's incredible how much money I can save buying on Wastood and as a bonus it helps fight climate change!".</p>
+                                </div>
+                                <div class="card-footer justify-content-center">
+                                    <a href="#" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                                    <a href="#" class="btn btn-link btn-just-icon"><i class="fa fa-linkedin"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="team-player">
+                            <div class="card card-plain">
+                                <div class="col-md-6 ml-auto mr-auto">
+                                    <img src="material-kit-master/assets/img/faces/kendall.jpg" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                                </div>
+                                <h4 class="card-title">Ana Pearson
+                                    <br>
+                                    <small class="card-description text-muted">Producer</small>
+                                </h4>
+                                <div class="card-body">
+                                    <p class="card-description">Ana has been selling her organic vegetables on our platform for 2 years now. "It's a great opportunity for me to build a local customer base and have a positive impact on the planet at the same time".</p>
+                                </div>
+                                <div class="card-footer justify-content-center">
+                                    <a href="#" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                                    <a href="#" class="btn btn-link btn-just-icon"><i class="fa fa-instagram"></i></a>
+                                    <a href="#" class="btn btn-link btn-just-icon"><i class="fa fa-facebook-square"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="section section-contacts">
+            <div class="row">
+                <div class="col-md-8 ml-auto mr-auto">
+                    <h2 class="text-center title">We want your feedback</h2>
+                    <h4 class="text-center description">We welcome any suggestion on how to improve our website, so please do not hesitate to send us an email.</h4>
+                    <form class="contact-form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="bmd-label-floating">Your Name</label>
+                                    <input type="email" class="form-control">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="bmd-label-floating">Your Email</label>
+                                    <input type="email" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleMessage" class="bmd-label-floating">Your Message</label>
+                            <textarea type="email" class="form-control" rows="4" id="exampleMessage"></textarea>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4 ml-auto mr-auto text-center">
+                                <button class="btn btn-primary btn-raised">
+                                    Send Message
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require('footer.php');
+?>
+</body>
+</html>
