fe36b53b90cb205d7aad400f70f6230dd71eae7a
Krysr <57724604+Krysr@users.noreply.github.com>
2019-12-04 15:04:48 +0000

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,12 +1,11 @@
+
 <?php
 require('connect.php');
-
 $categoryType = "";
 $mainArea = "";
 $userLocation = "";
 $userLatitude = "";
 $userLongitude = "";
-
 if(isset($_POST['category'])) {
     $categoryType = $_POST['category'];
 }
@@ -26,10 +25,8 @@ function coordsToDistance($latA, $longA, $latB, $longB)
 {
     $kmsInDeg = 111;
     $avgLat = ($latA + $latB) / 2;
-
     $deltaLat = abs($latA - $latB) * $kmsInDeg;
     $deltaLong = abs($longA - $longB) * cos($avgLat * (pi() / 180)) * $kmsInDeg;
-
     $distance = sqrt(($deltaLat * $deltaLat) + ($deltaLong * $deltaLong));
     return $distance;
 }
@@ -67,8 +64,7 @@ function coordsToDistance($latA, $longA, $latB, $longB)
     <div class="container">
         <div class="section text-center">
             <div class="row">
-                <div class="col-8 ml-auto mr-auto ">
-
+                <div class="col-md-8 ml-auto mr-auto ">
                     <?php
                     function select(){
                         return "SELECT * FROM `products`";
@@ -83,7 +79,7 @@ function coordsToDistance($latA, $longA, $latB, $longB)
                         $width = 200;
                         for ($j=0; $j < count($array); $j = $j + 3) {
                             echo "<tr>";
-                            echo "<div class='col'>";
+                            echo "<div class='d-sm-table-row'>";
                             for($c=0; $c < 3; $c++) {
                                 if(!empty($array[$j + $c])) {
                                     $distance = number_format($array[$j + $c]['distance'], 1);
@@ -96,12 +92,12 @@ function coordsToDistance($latA, $longA, $latB, $longB)
                                     $address = str_replace(' ', '+', $address);
                                     $url ="https://maps.google.com.au/maps?q=". $address;
                                     if($distance != -1) {
-                                        echo "<div class='card' style='width: 20rem;'>
-                              <td><img class='card-img-top' style='max-height: 300px;' src='data:image/jpeg;base64," . base64_encode($image) . "'/>" . "<div class='card-body'><p class='card-text'> $description </p><del class='card-text'> Originally: £ $genPrice </del></p><p class='card-text'> Now: £ $price </p><p id='distance'>$distance km</p>  <a href=$url onclick=\"return !window . open(this . href, 'Google', 'width=800,height=800')\" target=\"_blank\"> View location map </a>  <button class='btn btn-primary' onclick='addToBasket($id)'> Add to basket </button></div></div></td>";
+                                        echo "<div class='card-group'>
+                              <td class='d-md-table-cell'><img class='card-img-top' style='max-height: 300px;' src='data:image/jpeg;base64," . base64_encode($image) . "'/>" . "<div class='card-body'><p class='card-text'> $description </p><del class='card-text'> Originally: £ $genPrice </del></p><p class='card-text'> Now: £ $price </p><p id='distance'>$distance km</p>  <a href=$url class='btn btn-primary' onclick=\"return !window . open(this . href, 'Google', 'width=800,height=800')\" target=\"_blank\"> View location map </a>  <button class='btn btn-primary' onclick='addToBasket($id)'> Add to basket </button></div></div></td>";
                                     }
                                     else {
-                                        echo "<div class='card' style='width: 20rem;'>
-                              <td><img class='card-img-top' style='max-height: 300px;' src='data:image/jpeg;base64," . base64_encode($image) . "'/>" . "<div class='card-body'><p class='card-text'> $description </p><del class='card-text'> Originally: £ $genPrice </del></p><p class='card-text'> Now: £ $price </p><p id='distance'></p> <a href=$url onclick=\"return !window . open(this . href, 'Google', 'width=800,height=800')\" target=\"_blank\"> View location map </a> <button class='btn btn-primary' onclick='addToBasket($id)'> Add to basket </button></div></div></td>";
+                                        echo "<div class='card-group'>
+                              <td class='d-md-table-cell'><img class='card-img-top' style='max-height: 300px;' src='data:image/jpeg;base64," . base64_encode($image) . "'/>" . "<div class='card-body'><p class='card-text'> $description </p><del class='card-text'> Originally: £ $genPrice </del></p><p class='card-text'> Now: £ $price </p><p id='distance'></p> <a href=$url class='btn btn-primary' onclick=\"return !window . open(this . href, 'Google', 'width=800,height=800')\" target=\"_blank\"> View location map </a> <button class='btn btn-primary' onclick='addToBasket($id)'> Add to basket </button></div></div></td>";
                                     }
                                 }
                             }
@@ -113,12 +109,11 @@ function coordsToDistance($latA, $longA, $latB, $longB)
                     }
                     ?>
                     </br>
-                    <div class="col-4 ml-auto mr-auto" >
+                    <div class="col-8 ml-auto mr-auto" >
                         <form name="filterForm" method='post' action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-                            <div class="align-content-between">
                                 <input type="hidden" name="userLatitude" id="userLatitude">
                                 <input type="hidden" name="userLongitude" id="userLongitude">
-                                <div class="bmd-form-group">
+                                <div class="input-group">
                                     <select name="category" class="custom-select">
                                         <option value="auto" selected disabled>Sort by Category</option>
                                         <option value="all" <?php if(isset($_POST["category"]) && $categoryType == "all"){echo "selected";} ?>> All </option>
@@ -133,36 +128,29 @@ function coordsToDistance($latA, $longA, $latB, $longB)
                                         <option value="ready_meal" <?php if(isset($_POST["category"]) && $categoryType == "ready_meal"){echo "selected";} ?>> Ready meals </option>
                                         <option value="drinks" <?php if(isset($_POST["category"]) && $categoryType == "drinks"){echo "selected";} ?>> Drinks </option>
                                     </select>
+                                    <input type="text" name="userLocation" id="userLocation" class="form-control" style="margin-left: 2.5rem" placeholder="Location" value="<?php echo $userLocation; ?>">
+                                        <input type="button" name="submitBtn" value="Filter" class="btn btn-primary" style="margin-left: 2.5rem" onclick="checkUserLocation()"><span id="userLocationMessage"></span>
                                 </div>
-                                <div class="bmd-form-group input-group">
-                                    <input type="text" name="userLocation" id="userLocation" class="form-control" placeholder="Location" value="<?php echo $userLocation; ?>">
-                                </div>
-                                <input type="button" name="submitBtn" value="Filter" class="btn btn-primary" onclick="checkUserLocation()"><span id="userLocationMessage"></span>
                             </div>
                         </form>
                     </div>
-                    <div class="mx-auto">
+                    <div>
                         <?php
                         $array = array();
-
                         if(($categoryType == "all") || empty($categoryType)) {
                             $sql = select();
                         } else {
                             $sql = selectWhereCategory($categoryType);
                         }
                         $result = $conn->query($sql);
-
                         if (!$result) {
                             die("Query failed.");
                         }
-
                         unset($array);
                         $array = array();
-
                         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                             $array[] = $row;
                         }
-
                         $distance = [];
                         if(!empty($userLatitude) && !empty($userLongitude)) {
                             for ($i = 0; $i < $result->rowCount(); $i++) {
